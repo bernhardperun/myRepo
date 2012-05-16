f61c6dfd0cde50e709c1d7361885e424271d8bf8
bernhard <bernhard@vostro1500-fortuneTiger>
2012-05-16 14:37:40 +0200

    first commit

---
+++ mytestProject/src/mytestProject.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : mytestProject.cpp
+// Author      : bernhardperun
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
